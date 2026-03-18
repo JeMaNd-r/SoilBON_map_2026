aaf85cd442428ee69e54f8b2ff1b4641546d32d1
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2026-03-18 14:32:17 +0100

    Update Create_map.R

--- Create_map.R
+++ Create_map.R
@@ -2,20 +2,20 @@
 #                                                         #
 #      Map sampling locations of Soil BON project         #
 #                 author: Romy Zeiss                      #
-#                   date: 2024-10-30                      #
-#           last updated: 2026-03-16                      #
+#                   date: 2026-03-16                      #
+#           last updated: 2026-03-18                      #
 #                                                         #
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - -#
 
 # requirements:
 # - SVG
-# - show sites
+# X- show sites
 # - large-enough point size
-# - no colors for protected/unprotected
-# - panel showing protected and unprotected site
-# - borders of countries (with hint about country border discussions)
+# X- no colors for protected/unprotected
+# X- panel showing protected and unprotected site
+# X- borders of countries (with hint about country border discussions)
 #   “The boundaries and names shown on this map do not imply official endorsement or acceptance by the project.”
-# - color scheme as in example
+# X- color scheme as in example
 
 
 # load packages
@@ -25,6 +25,10 @@ library(readxl)
 library(leaflet) # create interactive map
 library(htmlwidgets) # save interactive map
 library(htmltools) # add popups in interactive map
+#install.packages("ggmagnify", repos = c("https://hughjonesd.r-universe.dev", "https://cloud.r-project.org"))
+library(ggmagnify) # to add zooms
+library(magick) #to add icon in map
+library(svglite) # to save as SVG
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Load data ####
@@ -33,164 +37,213 @@ library(htmltools) # add popups in interactive map
 world_map <- ggplot2::map_data("world")
 
 # sampling sites
-data <- readxl::read_xlsx("../../Sampling_sites/field records/field_records_digitalized.xlsx",
+data_1 <- readxl::read_xlsx("field_records_digitized_campaign1.xlsx",
                           sheet = "Sheet1")
-data
+data_1
 
-# registered sites
-data_registered <- read_csv("../../Sampling_sites/SoilBON_sites_download_20241218.csv")
-data_registered
+data_2 <- readxl::read_xlsx("field_records_digitzed_campaign2.xlsx",
+                            sheet = "Sheet1")
+data_2
+
+# Soil BON logo
+logo_soilbon <- "D:/Bilder/Sonstige Bilder/Logos/Soil_BON.jpg"
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Clean data ####
 
-data <- data %>%
-  mutate("Longitude" = as.double(ifelse(longitude == "na", NA, longitude)),
-         "Latitude" = as.double(ifelse(latitude == "na", NA, latitude)),
-         "Conservation area" = ifelse(`conservation area` == "na" |
-                                        `conservation area` == "cloudy" |
-                                        `conservation area` == "(yes)", 
-                                      NA, 
-                                      `conservation area`),
-         "Conservation area" = ifelse(`Conservation area` == "yes", "Protected site",
-                                      ifelse(`Conservation area` == "no", "Unprotected site",
-                                             `Conservation area`)))
+# rename conservation area column in data_1
+data_1 <- data_1 %>% rename(conservation_area = `conservation area`)
 
-#data %>% filter(`conservation area` == "cloudy") #HU007
-#data %>% filter(is.na(`Conservation area`)) %>% pull(ID) ## in case needed
-# "AR032" "CA034" "EC096" "EC097" "EC098" "EC099" "FR001" "FR002" "HU007" "IQ004" "UY001" "UY002" "UY003" "UY004" "UY005" "UY006"
-#data %>% filter(`conservation area` == "na") %>% pull(ID) ## in case needed
-# "CA034" "EC096" "EC097" "EC098" "EC099" "FR001" "FR002" "IQ004" "UY001" "UY002" "UY003" "UY004" "UY005" "UY006"
+f_clean_data <- function(data) { 
+  data <- data %>%
+    mutate("Longitude" = as.double(ifelse(longitude == "na", NA, longitude)),
+           "Latitude" = as.double(ifelse(latitude == "na", NA, latitude)),
+           "Conservation area" = ifelse(conservation_area == "na" |
+                                          conservation_area == "cloudy" |
+                                          conservation_area == "(yes)", 
+                                        NA, 
+                                        conservation_area),
+           "Conservation area" = ifelse(`Conservation area` == "yes", "Protected site",
+                                        ifelse(`Conservation area` == "no", "Unprotected site",
+                                               `Conservation area`)))
+  return(data)
+}
+
+clean_1 <- f_clean_data(data_1)
+clean_2 <- f_clean_data(data_2)
+
+table(clean_1$`Conservation area`)
+table(clean_2$`Conservation area`)
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Merge data from both sampling campaigns ####
+
+# function to filter and select relevant rows and columns
+f_subset <- function(data) {
+  data <- data %>%
+    filter(!is.na(Latitude) & !is.na(Longitude)) %>%
+    dplyr::select(Latitude, Longitude, `Conservation area`) %>%
+    unique()
+  return(data)
+}
+
+# subset both datasets
+subset_1 <- f_subset(clean_1) %>% 
+  mutate("Campaign_1" = 1) #498 of 523
+subset_2 <- f_subset(clean_2) %>% 
+  mutate("Campaign_2" = 1) #227 of 237
+
+# merge datasets
+data <- subset_1 %>%
+  full_join(subset_2)
+data #674 (instead of 227+498 = 725 unique sites)
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Fix parameters ####
+
+# number of sampling sites
+n_sites <- nrow(data); n_sites
+
+# points to zoom in: Unprotected site, Protected site
+data <- data %>%
+  mutate("Sample_point" = ifelse(round(Longitude) == -48 &    #-48
+                                   round(Latitude) == -16, 1, #-16
+                                        NA))
+sample_points <- data %>% 
+  filter(Sample_point==1) %>%
+  dplyr::select(Latitude, Longitude, `Conservation area`)
+sample_points
+
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Plot site map ####
 
-ggplot()+
+p <- ggplot()+
   geom_map(data = world_map, map = world_map, 
            aes(map_id = region),  show.legend = FALSE, 
            fill="white", color = "grey80", linewidth = 0.15)+
-  #scale_x_continuous(limits = c(-180, 180), expand = c(0,0))+
-  #scale_y_continuous(limits = c(-90, 90), expand = c(0,0))+
-  # geom_point(data = data %>% filter(!is.na(`Conservation area`)), ### !! warning: this is only based on the field record, not registered sites
-  #            aes(x = Longitude, y = Latitude,
-  #                color = `Conservation area`),
-  #            alpha = 0.5)+
-  # scale_color_manual(values = c("Protected site" = "#018571", "Unprotected site" = "#a6611a"),
-  #                    na.value = "black")+
+  scale_x_continuous(limits = c(-180, 180), expand = c(0,0))+
+  scale_y_continuous(limits = c(-90, 90), expand = c(0,0))+
+  #geom_point(aes(x = sample_points[,1], y = sample_points[,2]))+
+  geom_point(data = data,
+             aes(x = Longitude, y = Latitude),
+             alpha = 1, color = "#018571")+
+  geom_point(data = data %>% filter(!is.na(`Conservation area`)) %>%
+               filter(Sample_point==1), 
+             aes(x = Longitude, y = Latitude,
+                 color = `Conservation area`),
+             alpha = 1)+
+  scale_color_manual(values = c("Protected site" = "#019900", "Unprotected site" = "#a6611a"),
+                     na.value = "black")+
+  geom_text(aes(x = -35, y = 22, #x = -15, y = -18 for below
+                label = Sys.Date()),
+            hjust = 0.5, vjust = 0.5)+
+  geom_text(aes(x = -4, y = -33, #x = -15, y = -18 for below
+                label = paste0("n = ", n_sites, " sites")),
+            hjust = 0.5, vjust = 0.5)+
+  ggimage::geom_image(
+    aes(x = -40, y = 30, image = logo_soilbon),
+    size = 0.3,
+    hjust = 0.5)+
+  ggmagnify::geom_magnify(
+    aes(from = list(c(xmin = sample_points %>% pull(Longitude) %>% min() -1,
+                      xmax = sample_points %>% pull(Longitude) %>% max() +1,
+                 ymin = sample_points %>% pull(Latitude) %>% min() -2,
+                 ymax = sample_points %>% pull(Latitude) %>% max() +2)), #(xmin, xmax, ymin, ymax)
+        to = list(c(-17, -12,
+                  -25, -10))),
+    expand = 1,
+    scale.inset = 0.02,
+    colour = "grey80",
+    proj.linetype = 0, 
+    target.linetype = "dotted",
+    inset.linetype = "dotted")+
+  geom_text(aes(x = -18, y = -17, #x = -15, y = -18 for below
+                label = paste0("                      Protected\n\n\nUnprotected")),
+            hjust = 0.5, vjust = 0.5,
+            size = 2)+
+  labs(caption = "The boundaries shown on this map do not imply official endorsement or acceptance by the project.")+
   coord_map(projection = "ortho", orientation = c(10, -35, 23.5))+ # Ortho= make a globe, and orientation is for setting the center of the map
   theme_bw()+
   theme(panel.background = element_rect(fill= "aliceblue"),
+        legend.position = "none",
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         panel.border = element_blank(),
-        legend.title = element_blank())
+        plot.caption = element_text(size = 7),
+        legend.title = element_blank()); p
 
-ggsave(filename = paste0("../_Figures/Mapping/Map_sites_fieldrec_", gsub("-", "", Sys.Date()),".png"),
-       last_plot(),
-       height = 7, width = 15)
-
-
-# using leaflet ## interactive map
-plot_l <- leaflet() %>%
-  addTiles() %>%
-  setView(lng = -3.7, lat = 40.4, zoom = 1) %>%
-  addCircleMarkers(data = data %>% 
-                     rename(lng = Longitude, lat = Latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(is.na(`Conservation area`)) %>%
-                     dplyr::select(lng, lat, ID),
-                   color = "black",
-                   radius = 5,
-                   popup = ~htmlEscape(ID)) %>%
-  addCircleMarkers(data = data %>% 
-                     rename(lng = Longitude, lat = Latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(`Conservation area` == "Unprotected site") %>%
-                     dplyr::select(lng, lat, ID),
-                   color = "#a6611a",
-                   radius = 5,
-                   popup = ~htmlEscape(ID)) %>% 
-  addCircleMarkers(data = data %>% 
-                     rename(lng = Longitude, lat = Latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(`Conservation area` == "Protected site") %>%
-                     dplyr::select(lng, lat, ID),
-                   color = "#018571",
-                   radius = 5,
-                   popup = ~htmlEscape(ID)) 
-plot_l
-
-saveWidget(plot_l, file = paste0("../_Figures/Mapping/Map_sites_fieldrec_interactive_",
-                                 gsub("-", "", Sys.Date()),".html"))
-
+ggsave(filename = paste0("Map_sites_globe_", gsub("-", "", Sys.Date()),".png"),
+       p,
+       height = 5.65, width = 5.65,
+       dpi = 600)
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-## Plot registered site map ####
+## Splayed globe ####
 
-ggplot()+
+# points to zoom in: Unprotected site, Protected site
+data <- data %>%
+  mutate("Sample_point" = ifelse(round(Longitude) == -91 &    #-48, -70, -91
+                                   round(Latitude) == 15, 2, #-16, -21, -4, 15
+                                 NA))
+sample_points_2 <- data %>% 
+  filter(Sample_point==2) %>%
+  dplyr::select(Latitude, Longitude, `Conservation area`)
+sample_points_2
+
+splay <- ggplot()+
   geom_map(data = world_map, map = world_map, 
            aes(map_id = region),  show.legend = FALSE, 
            fill="white", color = "grey80", linewidth = 0.15)+
-  #scale_x_continuous(limits = c(-180, 180), expand = c(0,0))+
-  #scale_y_continuous(limits = c(-90, 90), expand = c(0,0))+
-  geom_point(data = data_registered, 
-             aes(x = longitude, y = latitude,
-                 color = protectedArea),
-             alpha = 0.5)+
-  scale_color_manual(values = c("TRUE" = "#018571", "FALSE" = "#a6611a"),
-                     na.value = "black")+
-  coord_map(projection = "ortho", orientation = c(10, -35, 23.5))+
+  scale_x_continuous(limits = c(-180, 180), expand = c(0,0))+
+  scale_y_continuous(limits = c(-90, 90), expand = c(0,0))+
+  geom_point(data = data, 
+             aes(x = Longitude, y = Latitude),
+             alpha = 1, color = "#018571")+
+  geom_point(data = data %>% filter(Sample_point == 2), 
+             aes(x = Longitude, y = Latitude),
+             color = "black", shape = 0, size = 5)+
+  coord_map(projection = "mercator")+ # Ortho= make a globe, and orientation is for setting the center of the map
+  labs(caption = "The boundaries shown on this map do not imply official endorsement or acceptance by the project.")+
   theme_bw()+
   theme(panel.background = element_rect(fill= "aliceblue"),
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         panel.border = element_blank(),
-        legend.title = element_blank())
+        legend.title = element_blank()); splay
 
-ggsave(filename = paste0("../_Figures/Mapping/Map_sites_registered_", gsub("-", "", Sys.Date()),".png"),
+zoom <- ggplot()+
+  scale_x_continuous(limits = c(sample_points_2 %>% pull(Longitude) %>% min()-0.05, 
+                                sample_points_2 %>% pull(Longitude) %>% max()+0.05), expand = c(0,0))+
+  scale_y_continuous(limits = c(sample_points_2 %>% pull(Latitude) %>% min()-0.05,
+                                sample_points_2 %>% pull(Latitude) %>% max()+0.05), expand = c(0,0))+
+  geom_point(data = data %>% filter(Sample_point == 2), 
+             aes(x = Longitude, y = Latitude,
+                 color = `Conservation area`),
+             alpha = 1, size = 5)+
+  scale_color_manual(values = c("Protected site" = "#019900", "Unprotected site" = "#a6611a"),
+                     na.value = "black")+
+  geom_text(data = data %>% filter(Sample_point == 2), 
+            aes(x = Longitude, y = Latitude,
+                label = `Conservation area`), 
+            vjust = -2, size = 2)+
+  coord_map(projection = "mercator")+ # Ortho= make a globe, and orientation is for setting the center of the map
+  theme_bw()+
+  theme(axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        legend.position = "none"); zoom
+
+splay + patchwork::inset_element(zoom, left = 0.02,
+                                bottom = 0.15,
+                                right = 0.23,
+                                top = 0.45)
+
+
+ggsave(filename = paste0("Map_sites_splayed_", gsub("-", "", Sys.Date()),".svg"),
        last_plot(),
-       height = 7, width = 15)
+       dpi = 600)
 
-
-# using leaflet
-plot_l <- leaflet() %>%
-  addTiles() %>%
-  setView(lng = -3.7, lat = 40.4, zoom = 1) %>%
-  addCircleMarkers(data = data_registered %>% 
-                     rename(lng = longitude, lat = latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(is.na(protectedArea)) %>%
-                     dplyr::select(lng, lat, id),
-                   color = "black",
-                   radius = 5,
-                   popup = ~htmlEscape(id)) %>%
-  addCircleMarkers(data = data_registered %>% 
-                     rename(lng = longitude, lat = latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(protectedArea == FALSE) %>%
-                     dplyr::select(lng, lat, id),
-                   color = "#a6611a",
-                   radius = 5,
-                   popup = ~htmlEscape(id)) %>%
-  addCircleMarkers(data = data_registered %>% 
-                     rename(lng = longitude, lat = latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(protectedArea == TRUE) %>%
-                     dplyr::select(lng, lat, id),
-                   color = "#018571",
-                   radius = 5,
-                   popup = ~htmlEscape(id)) %>%
-  addCircleMarkers(data = data_registered %>% 
-                     rename(lng = longitude, lat = latitude) %>%
-                     filter(!is.na(lng) & !is.na(lat)) %>%
-                     filter(id %in% c(1407, 1408)) %>%
-                     dplyr::select(lng, lat, id),
-                   color = "violet",
-                   radius = 5,
-                   popup = ~htmlEscape(id))
-plot_l
-
-saveWidget(plot_l, file = paste0("../_Figures/Mapping/Map_sites_registered_interactive_",
-                                 gsub("-", "", Sys.Date()),".html"))
